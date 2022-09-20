22b35747c792b4124cfca0ad207762febfeeccf1
j2zhao <j2zhao@uchicago.edu>
2022-09-20 13:49:55 -0500

    m

--- query_experiments.py
+++ query_experiments.py
@@ -14,9 +14,9 @@ import time
 
 def get_range(xsize, ysize, x, y):
     if x <= xsize:
-        y_size = int(xsize/x)*y_size
+        ysize = int(xsize/x)*ysize
     if y <= ysize:
-        x_size = int(ysize/y)*x_size
+        xsize = int(ysize/y)*xsize
 
     if x <= xsize:
         xstart = 0
@@ -36,7 +36,8 @@ def get_range(xsize, ysize, x, y):
 if __name__ == '__main__':
     
     sizes = [(1, 1), (10, 1), (100, 1), (1000, 1), (1000, 10), (1000, 100)]
-    experiments = [1, 10, 100, 1000, 10000, 100000]
+    #experiments = [1, 10, 100, 1000, 10000, 100000]
+    experiments = [1]
     num_steps = 5
     for k in range(len(experiments)):
         times = []
