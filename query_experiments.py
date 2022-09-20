edac14f5588c8e0aae4ca540e28551ec4eecb93f
j2zhao <j2zhao@uchicago.edu>
2022-09-20 12:57:24 -0500

    new new experiments

--- query_experiments.py
+++ query_experiments.py
@@ -32,7 +32,7 @@ def compression_convert(folder1, folder2, num_steps, dfile, input2, images):
         if i not in images:
             raw_save(array, folder2, 'step{}_'.format(i), ids = ids, arrow = False)
         else:
-            raw_save(array, folder2, 'step{}_'.format(i), ids = ids, image = True)
+            raw_save(array, folder2, 'step{}_'.format(i), ids = ids, image = False)
     return final_shape
 
 def get_range(xsize, ysize, x, y):
@@ -44,8 +44,6 @@ def get_range(xsize, ysize, x, y):
     else:
         xstart = random.randrange(0, x - xsize)
         xmax = xstart + xsize - 1
-
-
     if y <= ysize:
         ystart = 0
         ymax = y -1
@@ -55,60 +53,60 @@ def get_range(xsize, ysize, x, y):
 
     return (xstart, xmax), (ystart, ymax)
 
+def make_compression(f2, f1, num_steps, folder_range = 20, input2 = [], images = []):
+    for i in range(folder_range):
+        folder2 = f2 + str(i)
+        folder1 = f1 + str(i)
+        try:
+            shutil.rmtree(folder2)
+        except OSError as e:
+            pass
+        os.mkdir(folder2)
+
+        x, y = compression_convert(folder1, folder2, num_steps, dfile = '.pickle', input2, images)
+        with open(os.path.join(folder2, 'x.pickle'), 'wb') as f:
+            pickle.dump(x, f)
+        with open(os.path.join(folder2, 'y.pickle'), 'wb') as f:
+            pickle.dump(y, f)
+
 if __name__ == '__main__':
-    times = []
-    experiment = 100000
-    for j in range(100):
-        #folder1 = 'compression_tests_2/numpy_pipeline' + str(j)
-        folder1 = ''
-        folder2 = 'storage/raw' + str(j)
-        if folder1 != '':
-            try:
-                shutil.rmtree(folder2)
-            except OSError as e:
-                pass
-            os.mkdir(folder2)
-        print(folder2)
-        num_steps = 5
-        input2 = []
-        images = []
-        dfile = '.pickle'
-        xsize = 1000
-        ysize = 100
-        if folder1 != '':
-            x, y = compression_convert(folder1, folder2, num_steps, dfile, input2, images)
-            with open(os.path.join(folder2, 'x.pickle'), 'wb') as f:
-                pickle.dump(x, f)
-            with open(os.path.join(folder2, 'y.pickle'), 'wb') as f:
-                pickle.dump(y, f)
-            print('hello')
-        else:
+    #folder1 = 'compression_tests_2/numpy_pipeline' + str(j)
+    sizes = [(1, 1), (10, 1), (100, 1), (1000, 1), (1000, 10), (1000, 100)]
+    experiments = [1, 10, 100, 1000, 10000, 100000]
+    num_steps = 5
+    for k in len(experiments):
+        times = []
+        xsize = sizes[k][0]
+        ysize = size[k][1]
+        experiment = experiments[k]
+        for j in range(20):
+            # get folder name and last size
+            folder2 = 'storage/raw' + str(j)
+            print(folder2)
             with open(os.path.join(folder2, 'x.pickle'), 'rb') as f:
                 x = pickle.load(f)
             with open(os.path.join(folder2, 'y.pickle'), 'rb') as f:
                 y = pickle.load(f)
-        
-        pranges = [get_range(xsize, ysize, x, y)]
-        tnames = []
-        for i in range(num_steps):
-            tname = 'step{}_1'.format(i)
-            tnames.append(tname)
-        tnames.reverse()
-        #result = query_comp(pranges, folder2, tnames, absolute = False, merge = True, dtype = 'arrow')
-        #result = quer
-        # y_invertedlist(pranges, folder2, tnames, dtype = 'arrow')
-        #print(pranges)
-        start = time.time()
-        result = query_one2one(pranges, folder2, tnames, backwards = True, dtype = 'csv')
-        end = time.time()
-        times.append(end - start)
-        ##result = query_invertedlist(pranges, folder2, tnames, dtype = 'arrow')
-        #print(result)
-    times = np.asarray(times)
-    avg = np.average(times)
-    std = np.std(times)
-    print('average time')
-    print(avg)
-    print(std)
-    np.save('query_results/raw_times{}.npy'.format(experiment), times)
+            # get ranges and step names
+            pranges = [get_range(xsize, ysize, x, y)]
+            tnames = []
+            for i in range(num_steps):
+                tname = 'step{}_1'.format(i)
+                tnames.append(tname)
+            tnames.reverse()
+            
+            # get query results
+            start = time.time()
+            result = query_one2one(pranges, folder2, tnames, backwards = True, dtype = 'csv')
+            end = time.time()
+            times.append(end - start)
+            #result = query_comp(pranges, folder2, tnames, absolute = False, merge = True, dtype = 'arrow')
+        times = np.asarray(times)
+        avg = np.average(times)
+        std = np.std(times)
+        print('finished experiment: {}'.format(experiment))
+        print('average time: {}'.format(avg))
+        print('std time: {}'.format(std))
+        print(avg)
+        np.save('query_results/raw_times{}.npy'.format(experiment), times)
     
