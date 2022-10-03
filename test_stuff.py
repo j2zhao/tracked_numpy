0644a4c708183b52d8aa02147432c78679d8a7dc
j2zhao <j2zhao@uchicago.edu>
2022-10-02 21:24:00 -0500

    ah

---
+++ test_stuff.py
@@ -0,0 +1,31 @@
+import numpy as np
+from query_compression import *
+from query_dslog import *
+
+def load_parquet(folder):
+    tables = {}
+    for name in os.listdir(folder):
+        if name.endswith('.parquet'):
+            dire = os.path.join(folder, name)
+            nm = name.split('.')[0]
+            tables[nm] = pq.read_table(dire)
+    return tables
+            # get folder name and last size
+folder2 = 'storage/dslog_giz18'
+            #folder2 = 'storage/turbo' + str(j)
+            # with open(os.path.join(folder2, 'x.pickle'), 'rb') as f:
+            #     x = pickle.load(f)
+            # with open(os.path.join(folder2, 'y.pickle'), 'rb') as f:
+            #     y = pickle.load(f)
+            # get ranges and step names
+#pranges = [((520, 520), (955, 965))]
+tnames = ['step3_for1']
+
+tables = load_parquet(folder2)
+table = tables['step3_for1']
+print(table)
+
+            #tnames.reverse()
+            # get query results
+#query_comp(pranges, folder2, tnames, merge = True, dtype = 'arrow')
+            #result = query_one2one(pranges, folder2, tnames, backwards = False, dtype = 'arrow')
