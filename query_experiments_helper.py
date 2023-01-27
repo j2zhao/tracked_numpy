240e6e91264e30d6b336f24fb7db981d529738a2
j2zhao <j2zhao@uchicago.edu>
2023-01-27 16:13:09 -0500

    h raw

--- query_experiments_helper.py
+++ query_experiments_helper.py
@@ -27,7 +27,7 @@ def compression_convert(folder1, folder2, num_steps, dfile, input2):
             ids = [1]
         else:
             ids = [1,2]
-        comp_rel_save(array, folder2, 'step{}_'.format(i), image = False, arrow = True, gzip = True)
+        #comp_rel_save(array, folder2, 'step{}_'.format(i), image = False, arrow = True, gzip = True)
         #column_save(array, folder2, 'step{}_'.format(i), temp_path = './temp', ids = ids)
         #raw_save(array, folder2, 'step{}_'.format(i), ids = ids, arrow = False)
         #gzip_save(array, folder2, 'step{}_'.format(i), ids = ids, arrow = True)
