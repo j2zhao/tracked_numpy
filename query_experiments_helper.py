dfd2ea381c8f93de8d7077f94c442c2af6f0c197
j2zhao <j2zhao@uchicago.edu>
2023-01-27 13:24:58 -0500

    h raw

--- query_experiments_helper.py
+++ query_experiments_helper.py
@@ -29,7 +29,7 @@ def compression_convert(folder1, folder2, num_steps, dfile, input2):
             ids = [1,2]
         #comp_rel_save(array, folder2, 'step{}_'.format(i), image = False, arrow = True, gzip = True)
         #column_save(array, folder2, 'step{}_'.format(i), temp_path = './temp', ids = ids)
-        raw_save(array, folder2, 'step{}_'.format(i), ids = ids, arrow = True)
+        raw_save(array, folder2, 'step{}_'.format(i), ids = ids, arrow = False)
         #gzip_save(array, folder2, 'step{}_'.format(i), ids = ids, arrow = True)
         #else:
             #raw_save(array, folder2, 'step{}_'.format(i), ids = ids, image = False, arrow=True)
@@ -78,7 +78,7 @@ def make_compression_relational(f2, f1, num_steps):
 if __name__ == '__main__':
     #print('hello 2')
     #folder1 = 'compression_tests_2/relational_pipeline'
-    folder2 = 'storage_10/numpy_raw'
+    folder2 = 'storage_10/numpy_no_pq'
     folder1 = 'compression_tests_2/numpy_pipeline_10_'
     folder_range = list(range(20))
     #folder_range = []
