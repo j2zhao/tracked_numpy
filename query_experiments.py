9e1c667b113ae3566b5ace6fdee1892c882b877e
j2zhao <j2zhao@uchicago.edu>
2023-01-28 16:07:45 -0500

    t

--- query_experiments.py
+++ query_experiments.py
@@ -104,6 +104,6 @@ def query_experiemnts_pipeline(shape = [1080, 1920], folder2 = 'storage_pipeline
 if __name__ == '__main__':
     # query_experiments_numpy(shape = [9, 1000000], sizes = [(1, 1), (10, 1), (100, 1), (1000, 1), (1000, 10), (1000, 100)], \
     #     experiments = [1, 10, 100, 1000, 10000, 100000], num_steps = 10, num_exp = 20, save_name = 'query_results_10/numpy_raw_results', folder_name = 'storage_10/numpy_no_pq', forward = False)
-    query_experiemnts_pipeline(shape = [1080, 1920], folder2 = 'storage_pipeline/image_dslog')
+    query_experiemnts_pipeline(shape = [1080, 1920], folder2 = './storage_pipeline/storage_image_raw')
     #query_experiemnts_pipeline(shape = [9, 1000000], folder2 = 'storage_pipeline/relational_raw')
     
