eafdc4ebdddfd0d0890f6178094d345d061ac625
j2zhao <j2zhao@uchicago.edu>
2023-01-28 18:31:46 -0500

    t

--- query_experiments.py
+++ query_experiments.py
@@ -93,6 +93,7 @@ def query_experiemnts_pipeline(shape = [1080, 1920], folder2 = 'storage_pipeline
             start = time.time()
             #result = query_comp(pranges, folder2, tnames, backward = False, merge = True, dtype = 'arrow')
             result = query_one2one(pranges, folder2, tnames, backwards = False, dtype = 'turbo')
+            print(result)
             end = time.time()
             raise ValueError()
             if len(result) != 0:
