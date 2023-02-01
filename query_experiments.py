5b488e9cb377578d0a632a414168eef8bf06bf7a
j2zhao <j2zhao@uchicago.edu>
2023-02-01 14:40:31 -0500

    t

--- query_experiments.py
+++ query_experiments.py
@@ -42,7 +42,6 @@ def query_experiments_numpy(shape, sizes, experiments, num_steps, num_exp, save_
         j = 0
         print(experiment)
         for j in range(num_exp):
-            j = 3
             # get folder name and last size
             folder2 = folder_name + str(j)
             x = shape[0]
@@ -59,11 +58,10 @@ def query_experiments_numpy(shape, sizes, experiments, num_steps, num_exp, save_
                 tnames.append(tname)
             # get query results
             start = time.time()
-            result = query_comp(pranges, folder2, tnames, backward = False, merge = False, dtype = 'arrow')
+            result = query_comp(pranges, folder2, tnames, backward = False, merge = True, dtype = 'arrow')
             #result = query_one2one(pranges, folder2, tnames, backwards = False, dtype = 'turbo')
             end = time.time()
             times.append(end - start)
-            raise ValueError()
                 
         times = np.asarray(times)
         avg = np.average(times)
@@ -110,7 +108,7 @@ if __name__ == '__main__':
     # query_experiments_numpy(shape = [1000, 100], sizes = [(1, 1), (10, 1), (100, 1), (1000, 1), (1000, 10), (1000, 100)], \
     #      experiments = [1, 10, 100, 1000, 10000, 100000], num_steps = 5, num_exp = 20, save_name = 'query_results_5/numpy_dslog_results', folder_name = 'storage_5/numpy_dslog', forward = True)
     query_experiments_numpy(shape = [1000, 100], sizes = [(1000, 100)], \
-         experiments = [100000], num_steps = 5, num_exp = 20, save_name = 'query_results_5/numpy_dslog_results', folder_name = 'storage_5/numpy_dslog', forward = True)
+         experiments = [100000], num_steps = 5, num_exp = 20, save_name = 'query_results_5/numpy_dslog_results', folder_name = 'storage_5/numpy_dslog_merge', forward = True)
     #query_experiemnts_pipeline(shape = [1080, 1920], folder2 = './storage_pipeline/storage_image_col')
     #query_experiemnts_pipeline(shape = [9, 1000000], folder2 = 'storage_pipeline/storage_relational_col')
     
