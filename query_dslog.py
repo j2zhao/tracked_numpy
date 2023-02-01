7c7acee45bffbc833e2d9dfa620f66ee6fe36001
j2zhao <j2zhao@uchicago.edu>
2023-01-31 22:07:25 -0500

    t

--- query_dslog.py
+++ query_dslog.py
@@ -240,6 +240,7 @@ def query_comp(pranges, folder, tnames, backward = False, absolute = False, merg
     for name in tnames:
         oranges = []
         print(name)
+        print(pranges)
         for prange in pranges:
             #print(prange)
             x1 = prange[0][0]
