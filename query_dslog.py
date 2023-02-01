975535d6a7708da75a519ea7b07076ce3b149262
j2zhao <j2zhao@uchicago.edu>
2023-01-31 22:11:03 -0500

    t

--- query_dslog.py
+++ query_dslog.py
@@ -255,8 +255,8 @@ def query_comp(pranges, folder, tnames, backward = False, absolute = False, merg
             # r = "SELECT DISTINCT * FROM arrow_table WHERE LEAST(COALESCE(output_x2, output_x1), {}) >= GREATEST(output_x1, {}) \
             #     AND LEAST(COALESCE(output_y2, output_y1), {}) >= GREATEST(output_y1, {})".format(x2, x1, y2, y1)
             #print(r)
-            df = con.execute("SELECT DISTINCT * FROM arrow_table WHERE LEAST(COALESCE(output_x2, output_x1), {}) >= GREATEST(output_x1, {}) \
-                AND LEAST(COALESCE(output_y2, output_y1), {}) >= GREATEST(output_y1, {})".format(x2, x1, y2, y1)).fetchdf()
+            df = con.execute("SELECT DISTINCT * FROM arrow_table WHERE LEAST(output_x2, {}) >= GREATEST(output_x1, {}) \
+                AND LEAST(output_y2), {}) >= GREATEST(output_y1, {})".format(x2, x1, y2, y1)).fetchdf()
             print('here 2')
             if not absolute and backward:
                 oranges += input_output(prange, df)
